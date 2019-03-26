4eb2a08592345743c92395a6b6921f79a197b49a
Jim Paine <jim@jimpaine.io>
2019-03-26 23:12:52 +0000

    added 4.0

---
+++ labs/4.0/README.md
@@ -0,0 +1,62 @@
+![infra as code with Terraform](/docs/images/banner.png)
+
+# Service Principal
+
+In this lab we will create a Service Prinicpal and which to use that for further labs. The importance is why we use a service principal and not in how it is created so we have already written the Terraform to create a Service Principal in the Azure AD tenant, give it access to the Azure Subscription we are working against and add the credentials into an Azure Key Vault for our CI / CD pipelines later on.
+
+## Step 1 - Create the Service Prinicipal
+
+```
+terraform init
+```
+
+```
+terraform apply -var="user=[azurelogin@email.com]"
+```
+
+So lets take a look through so we understand what it is doing.
+
+## Step 2 - Azure AD provider
+
+If you take a look in the main.tf file you will notice we are using a new provider called "AzureAD" This provider allows us to work with Azure AD without a need to use or understand subscriptions or resource groups. You can then see from this provider we create a few resources.
+
+Firstly we create an Azure application, this is needed as it is what we will use when we want to authenticate and for setting RBAC permissions against subscriptions and resources. We have to set a few core items that we won't be using but because the application can also be used for user flows and app to app authentications items like the uris are required.
+
+```
+resource "azuread_application" "lab" {
+  name                       = "terraclient"
+  homepage                   = "https://homepage"
+  identifier_uris            = ["https://uri"]
+  reply_urls                 = ["https://uri"]
+  available_to_other_tenants = false
+  oauth2_allow_implicit_flow = true
+}
+```
+
+Next we wrap this up in a service prinicipal. This is all a little messy from an Azure AD perspective but we need to do this to assign credtentials such as a secret to our application. So we simply wrap the application in a service principal at this point.
+
+```
+resource "azuread_service_principal" "lab" {
+  application_id = "${azuread_application.lab.application_id}"
+}
+```
+
+Because everything we do we want to be secure we generate a random pssword to use with our new service prinicpal, ideally we would then put this only into Key Vault but for the purpose of the lab we will also output it.
+
+```
+resource "random_string" "lab" {
+  length  = "32"
+  special = true
+}
+```
+
+And finally we set the password of our service prinicpal with the randomly generated password.
+
+```
+resource "azuread_service_principal_password" "lab" {
+  service_principal_id = "${azuread_service_principal.lab.id}"
+  value                = "${random_string.lab.result}"
+  end_date             = "2020-01-01T01:02:03Z"
+}
+```
+
---
+++ labs/4.0/main.tf
@@ -0,0 +1,123 @@
+provider "azurerm" {
+  version = "1.22"
+}
+
+provider "azuread" {
+  version = "0.2"
+}
+
+provider "random" {
+  version = "1.3"
+}
+
+resource "azurerm_resource_group" "lab" {
+  name     = "lab-core"
+  location = "northeurope"    
+}
+
+resource "random_id" "lab" {
+  keepers = {
+    resource_group = "${azurerm_resource_group.lab.name}"
+  }
+
+  byte_length = 2
+}
+
+resource "azuread_application" "lab" {
+  name                       = "terraclient"
+  homepage                   = "https://homepage"
+  identifier_uris            = ["https://uri${random_id.lab.dec}"]
+  reply_urls                 = ["https://uri${random_id.lab.dec}"]
+  available_to_other_tenants = false
+  oauth2_allow_implicit_flow = true
+}
+
+resource "azuread_service_principal" "lab" {
+  application_id = "${azuread_application.lab.application_id}"
+}
+
+resource "random_string" "lab" {
+  length  = "32"
+  special = true
+}
+
+resource "azuread_service_principal_password" "lab" {
+  service_principal_id = "${azuread_service_principal.lab.id}"
+  value                = "${random_string.lab.result}"
+  end_date             = "2020-01-01T01:02:03Z"
+}
+
+data "azurerm_client_config" "lab" {}
+
+resource "azurerm_role_assignment" "lab" {
+  scope                = "/subscriptions/${data.azurerm_client_config.lab.subscription_id}"
+  role_definition_name = "Owner"
+  principal_id         = "${azuread_service_principal.lab.id}"
+}
+data "azuread_user" "lab" {
+  user_principal_name = "${var.user}"
+}
+
+resource "azurerm_key_vault" "lab" {
+  name                = "lab${random_id.lab.dec}vault"
+  location            = "${azurerm_resource_group.lab.location}"
+  resource_group_name = "${azurerm_resource_group.lab.name}"
+  tenant_id           = "${data.azurerm_client_config.lab.tenant_id}"
+
+  sku {
+    name = "standard"
+  }
+
+  access_policy {
+    tenant_id = "${data.azurerm_client_config.lab.tenant_id}"
+    object_id = "${data.azuread_user.lab.id}"
+
+    key_permissions = []
+
+    secret_permissions = [
+      "list",
+      "set",
+      "get",
+    ]
+  }
+}
+
+output "client-id" {
+    value = "${azuread_application.lab.application_id}"
+}
+
+resource "azurerm_key_vault_secret" "client-id" {
+    name = "client-id"
+    value = "${azuread_application.lab.application_id}"
+    vault_uri = "${azurerm_key_vault.lab.vault_uri}"
+}
+
+output "client-secret" {
+  value = "${random_string.lab.result}"
+}
+
+resource "azurerm_key_vault_secret" "client-secret" {
+    name = "client-secret"
+    value = "${random_string.lab.result}"
+    vault_uri = "${azurerm_key_vault.lab.vault_uri}"
+}
+
+output "subscription-id" {
+  value = "${data.azurerm_client_config.lab.subscription_id}"
+}
+
+resource "azurerm_key_vault_secret" "subscription-id" {
+    name = "subscription-id"
+    value = "${data.azurerm_client_config.lab.subscription_id}"
+    vault_uri = "${azurerm_key_vault.lab.vault_uri}"
+}
+
+output "tenant-id" {
+  value = "${data.azurerm_client_config.lab.tenant_id}"
+}
+
+resource "azurerm_key_vault_secret" "tenant-id" {
+    name = "tenant-id"
+    value = "${data.azurerm_client_config.lab.tenant_id}"
+    vault_uri = "${azurerm_key_vault.lab.vault_uri}"
+}
