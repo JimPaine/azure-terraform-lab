79b61efc7dcd39b80874213d3c1f03941723256c
Jim Paine <jim@jimpaine.io>
2019-03-27 08:00:30 +0000

    removed dup

--- env/main.tf
+++ env/main.tf
@@ -1,16 +1,3 @@
-provider "azurerm" {
-  version = "1.23"
-}
-
-provider "azuread" {
-  version = "0.2"
-}
-
-
-provider "random" {
-  version = "1.3"
-}
-
 resource "azurerm_resource_group" "lab" {
   name     = "lab" 
   location = "westeurope"
--- env/providers.tf
+++ env/providers.tf
@@ -1,7 +1,16 @@
 provider "azurerm" {
   subscription_id = "${var.subscription-id}"
-  version         = "1.22"
+  version         = "1.23"
   client_id       = "${var.client-id}"
   client_secret   = "${var.client-secret}"
   tenant_id       = "${var.tenant-id}"
+}
+
+provider "azuread" {
+  version = "0.2"
+}
+
+
+provider "random" {
+  version = "1.3"
 }
